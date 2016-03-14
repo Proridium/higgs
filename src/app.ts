f47db8589e2fdb7ceb13de85f95d49e31e9f9d6c
Steve Hartzog <shartzog@gmail.com>
2016-03-13 22:04:11 -0400

    #1, #2: Add nav-skeleton boilerplate.

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ export class App {
   router: Router;
   
   configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+    config.title = 'Higgs';
     config.map([
       { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
       { route: 'users', name: 'users/index', moduleId: './users/index', nav: true, title:'Users' },
